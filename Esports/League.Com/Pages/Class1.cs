2bc7f51a7566fe5cfe6337034089b7d8c872b8c1
Unknown <rob.m@owletcare.com>
2018-08-02 20:07:07 -0600

    added schedule page object

--- Esports/League.Com/Pages/Class1.cs
+++ Esports/League.Com/Pages/Class1.cs
@@ -1,9 +1,45 @@
 ﻿using System;
+using OpenQA.Selenium;
+using System.Threading;
 
 namespace League.Com.Pages
 {
-    public class Class1
+    public class Schedule
+    {
+        IWebDriver _driver;
+        public readonly ScheduleMap Map;
+
+        public Schedule(IWebDriver driver)
+        {
+            _driver = driver;
+            Map = new ScheduleMap(driver);
+
+        }
+
+        public void GoTo()
+        {
+            Map.Schedule.Click();
+            WaitForPageLoad();
+        }
+
+        public void WaitForPageLoad()
+        {
+            Thread.Sleep(1000); 
+        }
+    }
+
+    public class ScheduleMap
     {
         
+        IWebDriver _driver;
+
+        public ScheduleMap(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+            public IWebElement Schedule => _driver.FindElement(By.CssSelector("[href='/en_US/na-lcs/na_2018_summer/schedule']"));
+
+            public IWebElement RegSeason => _driver.FindElement(By.CssSelector("[data-dropdown='drop-2']"));
     }
 }
