896abf011614becc49cd66464eb3704a91ce7d72
Unknown <robmiguel2@gmail.com>
2018-08-16 18:59:11 -0600

    esports menu

---
+++ Esports/League.Com/Pages/Base/EsportsMenu.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+
+namespace League.Com.Pages
+{
+    public class EsportsMenu
+    {
+        readonly IWebDriver _driver;
+        public readonly EsportsMenuMap Map;
+
+        public EsportsMenu(IWebDriver driver)
+        {
+            _driver = driver;
+            Map = new EsportsMenuMap(driver);
+        }
+
+        public void GotoHome()
+        {
+            Map.HomeTab.Click();
+        }
+
+        public void GotoNALCS()
+        {
+            Map.NALCSTab.Click();
+        }
+    }
+
+    public class EsportsMenuMap
+    {
+        readonly IWebDriver _driver;
+
+        public EsportsMenuMap(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public IWebElement HomeTab => _driver.FindElement(By.XPath("(//a[text()='HOME'])[last()]"));
+        public IWebElement NALCSTab => _driver.FindElement(By.XPath("//a[text()='NA LCS']"));
+    }
+}
