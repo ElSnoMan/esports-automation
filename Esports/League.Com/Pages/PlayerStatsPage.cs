e3a479ea587748de992c89e1a5ae6e603618a495
Unknown <jraysparks@gmail.com>
2018-08-16 20:48:12 -0600

    Building out the Player Stats Page

--- Esports/League.Com/Pages/PlayerStatsPage.cs
+++ Esports/League.Com/Pages/PlayerStatsPage.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
+using Framework.Model;
 using OpenQA.Selenium;
 
 namespace League.Com.Pages
@@ -19,6 +22,28 @@ namespace League.Com.Pages
         {
             Map.StatsTab.Click();
             WaitForPageLoad();
+        }
+
+        public PlayerStats GetPlayerName(string name)
+        {
+            var row = Map.PlayerRows.FirstOrDefault(r => r.Text.Contains(name));
+            var cells = row.FindElements(By.TagName("td"));
+            var player = new PlayerStats();
+
+            player.Name = cells[0].Text;
+            player.Team = cells[1].Text;
+            player.Position = cells[2].Text;
+            player.KDA = Convert.ToDouble(cells[3].Text);
+            player.Kills = Convert.ToInt32(cells[4].Text);
+            player.Deaths = Convert.ToInt32(cells[5].Text);
+            player.Assists = Convert.ToInt32(cells[6].Text);
+            player.KillParticipation = Convert.ToDouble(cells[7].Text);
+            player.CsPerMin = Convert.ToDouble(cells[8].Text);
+            player.Cs = Convert.ToInt32(cells[9].Text);
+            player.MinutesPlayed = Convert.ToInt32(cells[10].Text);
+            player.GamesPlayed = Convert.ToInt32(cells[11].Text);
+
+            return player;
         }
 
         public void SelectSplit(string name)
@@ -65,5 +90,7 @@ namespace League.Com.Pages
         public IWebElement SplitMenu => _driver.FindElement(By.Id("drop-1"));
         public IWebElement StageDropDown => _driver.FindElement(By.XPath("//a[@data-dropdown='drop-2']"));
         public IWebElement StageMenu => _driver.FindElement(By.Id("drop-2"));
+
+        public IList<IWebElement>PlayerRows  => _driver.FindElements(By.XPath("//tbody//tr"));
     }
 }
