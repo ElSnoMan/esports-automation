30e08d86af46c4911283454d0316ecc020f1489d
Tucker Weathers <tucker.weathers.127@gmail.com>
2018-09-06 18:56:32 -0600

    Added LolEsportsHomePage POM

--- Esports/League.Com/Pages/LolEsportsHomePage.cs
+++ Esports/League.Com/Pages/LolEsportsHomePage.cs
@@ -1,10 +1,45 @@
 ﻿using System;
+using League.Com.Pages.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
 namespace League.Com.Pages
 {
-    public class HomePage
-    {
-        public HomePage()
-        {
-        }
-    }
+	public class LolEsportsHomePage : PageBase
+	{
+		readonly IWebDriver _driver;
+		readonly WebDriverWait _wait;
+		public readonly LolEsportsHomePageMap Map;
+
+		public LolEsportsHomePage(IWebDriver driver, WebDriverWait wait) : base(driver)
+		{
+			_driver = driver;
+			_wait = wait;
+			Map = new LolEsportsHomePageMap(driver);
+		}
+
+		public void Goto()
+		{
+			EsportsMenu.GotoHome();
+		}
+
+
+		public void WaitForPageLoad()
+		{
+			_wait.Until((drvr) => EsportsMenu.Map.HomeLink.Displayed);
+		}
+
+
+	}
+	public class LolEsportsHomePageMap
+
+	{
+		readonly IWebDriver _driver;
+
+		public LolEsportsHomePageMap(IWebDriver driver)
+		{
+			_driver = driver;
+		}
+	}
 }
+
